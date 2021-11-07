f044fb9ab466173ed3bf60b273042ee495a96a69
Alexander Semenov <EuroTractor@yandex.ru>
2021-11-07 16:18:14 +0300

    Проект 4 НА СДАЧУ

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,25 @@
 const popupElement = document.querySelector('.popup');
-
 const closeButton = popupElement.querySelector('.popup__close-button');
-const header = document.querySelector('header');
-const navButton = document.querySelector('.header__nav-item');
-
-
+const navButton = document.querySelector('.profile__nav-item');
 function openPopup() {
-    popupElement.classList.remove('hidden');
+  popupElement.classList.add('popup_opened');
 }
-
 function closePopup() {
-    popupElement.classList.add('hidden');
+  popupElement.classList.remove('popup_opened');
 }
-navButton.addEventListener('click', openPopup)
+navButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
-closeButton.addEventListener('click', closePopup)
+
+
+let addButton = document.querySelector('.popup__btn-submit');
+let userName = document.querySelector('.profile__user-name');
+let usersHobby = document.querySelector('.profile__users-hobby');
+function renameName() {
+  let names = document.querySelector('.popup__text_type_name');
+  userName.textContent = (names).value;
+  let hobbies = document.querySelector('.popup__text_type_artist');
+  usersHobby.textContent = (hobbies).value;
+}
+addButton.addEventListener('click', renameName);
+addButton.addEventListener('click', closePopup);
