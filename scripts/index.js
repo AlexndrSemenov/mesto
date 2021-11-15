005b9c25faa6baeea0eb654a58ecf8f785a9d9f5
Alexander Semenov <EuroTractor@yandex.ru>
2021-11-15 12:40:20 +0300

    Проект 4 на СДАЧУ версия 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,29 @@
+const popupContent = document.querySelector('.popup__content');
 const popupElement = document.querySelector('.popup');
-const closeButton = popupElement.querySelector('.popup__close-button');
 const navButton = document.querySelector('.profile__nav-item');
-function openPopup() {
-  popupElement.classList.add('popup_opened');
-}
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
-}
-navButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-
-
-let addButton = document.querySelector('.popup__btn-submit');
+const closeButton = popupElement.querySelector('.popup__close-button');
 let userName = document.querySelector('.profile__user-name');
 let usersHobby = document.querySelector('.profile__users-hobby');
-function renameName() {
-  let names = document.querySelector('.popup__text_type_name');
-  userName.textContent = (names).value;
-  let hobbies = document.querySelector('.popup__text_type_artist');
-  usersHobby.textContent = (hobbies).value;
+let names = document.querySelector('.popup__text_type_name');
+let hobbies = document.querySelector('.popup__text_type_artist');
+
+function openPopup() { //функция открытия попап
+  names.value = userName.textContent;
+  hobbies.value = usersHobby.textContent;
+  popupElement.classList.add('popup_opened');
 }
-addButton.addEventListener('click', renameName);
-addButton.addEventListener('click', closePopup);
+
+function closePopup() { //функция закрытия попап без сохранения введенных данных
+  popupElement.classList.remove('popup_opened');
+}
+
+function renameName(evt) { //функция изменения данных на сайте через попап
+  evt.preventDefault() //Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
+  userName.textContent = names.value;
+  usersHobby.textContent = hobbies.value;
+  closePopup();
+}
+
+popupContent.addEventListener('submit', renameName);
+navButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
