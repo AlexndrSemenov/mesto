f6678ffd3e462b12584e54948d719daf230e582b
Alexander Semenov <EuroTractor@yandex.ru>
2021-12-27 12:04:56 +0300

    роект 7  С версия 3

--- scripts/Card.js
+++ scripts/Card.js
@@ -17,31 +17,33 @@ export class Card {
     return taskTemplate;
   }
 
+  _handleLike = () => {
+    this._element.querySelector('.photo-grid__heart').classList.toggle('photo-grid__heart_active');
+    }
+
+  _handleDeleteCard = () => {
+    this._element.querySelector('.photo-grid__delete').closest('.task').remove();
+    this._element = null;
+  }
+
+  _handleOpenImage = () => {
+    pictureInPopup.src = this._link;
+    pictureInPopup.alt = this._name;
+    popupPictureDiscription.textContent = this._name;
+    openPopup(popupPicture);
+  }
+
   generateCard() {
     this._element = this._getTemplate();
 
     // ставим лайк
-    const _handleLike = () => {
-      this._element.querySelector('.photo-grid__heart').classList.toggle('photo-grid__heart_active');
-    }
-    this._element.querySelector('.photo-grid__heart').addEventListener('click', _handleLike);
+    this._element.querySelector('.photo-grid__heart').addEventListener('click', this._handleLike);
     
     // удаляем карточку
-    const _handleDeleteCard = () => {
-      this._element.querySelector('.photo-grid__delete').closest('.task').remove();
-      this._element = null;
-    }
-    this._element.querySelector('.photo-grid__delete').addEventListener('click', _handleDeleteCard);
-
+    this._element.querySelector('.photo-grid__delete').addEventListener('click', this._handleDeleteCard);
 
     // показываем фото в попап
-    const handleOpenImage = () => {
-      pictureInPopup.src = this._link;
-      pictureInPopup.alt = this._name;
-      popupPictureDiscription.textContent = this._name;
-      openPopup(popupPicture);
-    }
-    this._element.querySelector('.photo-grid__image').addEventListener('click', handleOpenImage);
+    this._element.querySelector('.photo-grid__image').addEventListener('click', this._handleOpenImage);
 
     this._element.querySelector('.photo-grid__image').src = this._link;
     this._element.querySelector('.photo-grid__text').textContent = this._name;
